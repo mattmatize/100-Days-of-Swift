0488a623457215001a742a97735563e56078f744
Mateus Da Silva <mateusds.mds@gmail.com>
2020-12-03 20:01:12 -0500

    Day 01 of 100 Days of Swift progress

---
+++ Day 01/Day 01.playground/Contents.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+//Day 01
+
+//variables hold values that can change over time.
+
+//var str = "Hello, playground"
+//str  = "Goodbye"
+
+// Creating a new variable called currentShow and giving it the value Breaking Bad
+//var currentShow = "Breaking Bad"
+// Giving currentShow new values.
+//str = "Dark"
+//str = "The Queen's Gambit"
+
+//Integers
+var age = 38
+var population = 8_000_000
+//str (string) for letters
+//int (integer) for whole numbers
+
+//Multi-line
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+//Doubles
+var pi = 3.141
+// fractions creat a double instead of integers
+
+//booleans hold true or false
+var awesome = true
+
+
+//String Interpolation
+var score = 85
+var str = "Your score was \(score)"
+var results = "The test results are here: \(str)"
+
+//let creates constants
+let taylor = "swift"
+
+//Type annotations
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
