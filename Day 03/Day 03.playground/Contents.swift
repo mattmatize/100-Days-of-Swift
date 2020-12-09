44b9acd672a42d47b25b9574bde5d2a9dceabc7d
Mateus Da Silva <mateusds.mds@gmail.com>
2020-12-08 20:13:49 -0500

    Day 03 Start

---
+++ Day 03/Day 03.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//Operators
+let firstScore = 12
+let secondScore = 4
+
+let total = firstScore + secondScore
+let difference = firstScore - secondScore
+
+let product = firstScore * secondScore
+let divided = firstScore / secondScore
+let remainder = 13 % secondScore
+
+//Operator Overloading
+let meaningOfLife = 42
+let doubleMeaning = 42 + 42
+
+let fakers = "Fakers gonna "
+let action = fakers + "fake"
+
+let firstHalf = ["John", "Paul"]
+let secondHalf = ["George", "Ringo"]
+let Beatles = firstHalf + secondHalf
+
+//Compound Assignment Operators
+var score = 95
+score -= 5
+
+var quote = "The rain in Spain falls mainly on the "
+quote += "Spaniards"
+
+//Comparison Operators
+let theFirstScore = 6
+let theSecondScore = 4
+
+// == checks if two values are the same
+// != checks if two values are not the same
