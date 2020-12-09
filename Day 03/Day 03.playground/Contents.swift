30e5a7d75537a0a05aab642c6029a93c33962e35
Mateus Da Silva <mateusds.mds@gmail.com>
2020-12-08 20:39:51 -0500

    Day 03 Progress

--- Day 03/Day 03.playground/Contents.swift
+++ Day 03/Day 03.playground/Contents.swift
@@ -35,5 +35,83 @@ quote += "Spaniards"
 let theFirstScore = 6
 let theSecondScore = 4
 
-// == checks if two values are the same
-// != checks if two values are not the same
+// == checks two values are the same
+// != checks two values are not the same
+
+theFirstScore == theSecondScore
+theFirstScore != theSecondScore
+
+theFirstScore < theSecondScore
+theFirstScore >= theSecondScore
+
+//strings have a natural alphabetical order:
+"Taylor" <= "Swift"
+
+//Conditions
+let firstCard = 11
+let secondCard = 10
+
+if firstCard + secondCard == 2{
+    print("Aces – lucky!")
+} else if firstCard + secondCard == 21{
+    print("Blackjack!")
+} else {
+    print("Regular cards!")
+}
+
+//Combining Conditions
+let age1 = 18
+let age2 = 21
+
+//&& for both, || for either
+
+if age1 > 18 && age2 > 18{
+    print("Both are over 18!")
+}
+
+if age1 > 18 || age2 > 18{
+    print("One of them is over 18!")
+}
+
+//The Ternary Operator
+//Not only recommended
+let theFirstCard = 11
+let theSecondCard = 10
+print(theFirstCard == theSecondCard ? "Cards are the same" : "Cards are different")
+
+//Better way to write it using an If statement
+
+if theFirstCard == theSecondScore{
+    print("Cards are the same")
+} else {
+    print("Cards are different")
+}
+
+//Switch Statements
+let weather = "Sunny"
+
+switch weather{
+case "rain":
+    print("Bring an umbrella")
+case "snow":
+    print("Wrap up warm")
+case "sunny":
+    print("Wear sunscreen")
+default:
+    print("Enjoy your day!")
+}
+
+//Range Operators
+//the range 1..<5 contains the numbers 1, 2, 3, and 4, whereas the range 1...5 contains the numbers 1, 2, 3, 4, and 5.
+
+let grade = 85
+
+switch grade{
+case 0..<50:
+    print("You failed badly...")
+case 50..<85:
+    print("You did OK.")
+default:
+    print("You did great!")
+}
+
